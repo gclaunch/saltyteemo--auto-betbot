06f8706f341a1aaf5ed2ed655b71136bd74fb6b7
gclaunch <claunch.grant@gmail.com>
2021-03-22 08:25:07 -0500

    update for xxsaltbotxx anti-bot chat messages

--- bot.js
+++ bot.js
@@ -43,69 +43,72 @@ var timestampFile = `./timestamp/${BOT_USERNAME}.txt`;
 function onMessageHandler (channel, user, message, self) {
   if (self) { return; } // Ignore messages from myself
   
+  // Listen for bets starting and then bet after delay
+  if (message.includes('!red') || message.includes('!blue')) {
+    if (betstarted === 0) {
+      betstarted = 1;
+
+      (async () => {
+        await delay.range(154000, 156000); // Bet random times for more natural looking bets
+          
+        var betInt = math.round(math.random(500, 1500)); // Bet random amount since we're no longer balance tracking
+
+        if (betInt > 1) {
+          var betAmount = betInt;
+        } else {
+          var betAmount = 250;
+        }
+
+        if (red > blue) {
+          var betColor = 'blue';
+        } else {
+          var betColor = 'red';
+        }
+
+        if (betstarted === 1) {
+          client.say(channel, `!${betColor} ${betAmount}`);
+          betstarted = 2;
+        }
+
+        await delay(230000); // reset variables after delay
+
+        red = 0;
+        blue = 0;
+        betstarted = 0;
+        
+      })();
+    }
+  }
+
+  // Listen for Red bets
+  if (message.includes('!red')) {
+    var betsRed = parseInt(message.split(' ')[1]);
+    red = red + betsRed;
+  }
+
+  // Listen for Blue bets
+  if (message.includes('!blue')) {
+    var betsBlue = parseInt(message.split(' ')[1]);
+    blue = blue + betsBlue;
+  }
+
   if (user.username === userListener) { // Only listen to messages from this user
-  
-    // Listen for bets starting and then bet after delay
-    if (message.includes('bet submitted for')) {
-      if (betstarted === 0) {
-        betstarted = 1;
-
-        (async () => {
-          await delay.range(150000, 160000); // Bet random times for more natural looking bets
-          
-          fs.readFile(balanceFile, 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            
-            var betInt = math.round(data*0.015); // Bet 1.5% of stored balance
-
-            if (betInt > 1) { // Set in case of errors
-              var betAmount = betInt;
-            } else {
-              var betAmount = 250;
-            }
-
-            if (red > blue) { // Bet for red or blue, whichever has less bets
-              var betColor = 'blue';
-            } else {
-              var betColor = 'red';
-            }
-
-            if (betstarted === 1) { // Set to prevent multi betting
-              client.say(channel, `!${betColor} ${betAmount}`);
-            }
-          });
-          
-        })();
-      }
-    }
-
-    // Listen for Red bets
-    if (message.includes('RED bet submitted for')) {
-      var betsRed = parseInt(message.split(' ')[6]);
-      red = red + betsRed;
-    }
-
-    // Listen for Blue bets
-    if (message.includes('BLUE bet submitted for')) {
-      var betsBlue = parseInt(message.split(' ')[6]);
-      blue = blue + betsBlue;
-    }
 
     // Reset at end of betting round
-    if (message.includes('Betting has ended for this round')) {
+    if (message.includes('Betting has ended')) {
       red = 0;
       blue = 0;
       betstarted = 0;
     }
 
     // Reset if bets haven't started (in case of game crashes or early surrenders)
-    if (message.includes('Betting has not opened yet')) {
+    if (message.includes('Betting has not opened')) {
       red = 0;
       blue = 0;
       betstarted = 0;
     }
 
-    // Listen for my own bet, store balance, and reset globals after delay
+    // Listen for my own bet, store balance, and reset variables after delay
     if (message.includes(`@${BOT_USERNAME} - RED bet submitted for`) || message.includes(`@${BOT_USERNAME} - BLUE bet submitted for`)) {
       var balanceInt = parseInt(message.split(' ')[11]);
       fs.writeFile(balanceFile, balanceInt.toString(), function (err) {
